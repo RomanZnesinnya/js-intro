a828820b47c75f472d731435462eddaa886e77c2
romangrytsa <89747965+RomanZnesinnya@users.noreply.github.com>
2021-09-24 12:51:51 +0200

    Create task-01.js

---
+++ task-01.js
@@ -0,0 +1,15 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+function move (position, roll) {
+    return position+ roll*2;
+}
+
+function greet (name, owner) {
+    if (name === owner) return "Hello boss"; return "Hello guest";
+}
+
+function litres(time) {
+    return Math.floor(time * 0.5);
+}
